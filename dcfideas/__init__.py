d18626074f457a43ee68df92213eaaae68390b79
chelsea-designs <hello@chelsea-designs.com>
2022-06-28 17:54:37 +0000

    update database_uri to be postgresql

--- dcfideas/__init__.py
+++ dcfideas/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
@@ -11,6 +12,9 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT")=="True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri=uri.replace("postgres://", "postgresql://",1)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
