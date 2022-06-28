b239a2ca1ae25ad3c7018db935894ab890105096
chelsea-designs <hello@chelsea-designs.com>
2022-06-28 13:44:52 +0000

    typo in requirement.txt file should be plural

--- dcfideas/routes.py
+++ dcfideas/routes.py
@@ -5,7 +5,7 @@ from dcfideas.models import Strand, Idea
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
 
 @app.route("/strands")
 def strands():
