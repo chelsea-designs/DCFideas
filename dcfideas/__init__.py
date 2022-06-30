c4b7d9c4b54cef90afa8eef2174e3b03abad28e9
chelsea-designs <hello@chelsea-designs.com>
2022-06-30 07:50:12 +0000

    add mongo url

--- dcfideas/__init__.py
+++ dcfideas/__init__.py
@@ -1,13 +1,20 @@
 import os
 import re
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env  # noqa
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo=PyMongo(app)
 
 if os.environ.get("DEVELOPMENT")=="True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
