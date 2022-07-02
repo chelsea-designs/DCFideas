ae73ccec56976c42f371a39e1a0928d250f25e30
chelsea-designs <hello@chelsea-designs.com>
2022-07-02 14:17:14 +0000

    undo dburl change

--- dcfideas/__init__.py
+++ dcfideas/__init__.py
@@ -19,7 +19,7 @@ mongo = PyMongo(app)
 if os.environ.get("DEVELOPMENT")=="True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("DB_URL")
+    uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
