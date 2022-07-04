6fe98c2d1c97f68f3faba66da28c6fd47eb04889
chelsea-designs <hello@chelsea-designs.com>
2022-07-04 12:39:19 +0000

    update register route

--- dcfideas/routes.py
+++ dcfideas/routes.py
@@ -11,23 +11,26 @@ def home():
 def register():
     if request.method == "POST":
         # check if username already exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
-
+        existing_user = Users.query.filter(Users.user_name == \
+                                           request.form.get("username").lower()).all()
+        
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
-        #db.users.insert(users).values(username='username', password='password')
+        
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+        
+        db.session.add(user)
+        db.session.commit()
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
