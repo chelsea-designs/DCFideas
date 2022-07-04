33faa1759f26702b399250e51ddd9ea7d589c69c
chelsea-designs <hello@chelsea-designs.com>
2022-07-04 18:07:12 +0000

    whitespace around operator init file

--- dcfideas/__init__.py
+++ dcfideas/__init__.py
@@ -16,7 +16,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
-if os.environ.get("DEVELOPMENT")=="True":
+if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
