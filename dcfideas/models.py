a34de99c84e09d9f8dcf8247d5a8033e2f423c9c
chelsea-designs <hello@chelsea-designs.com>
2022-07-04 12:45:21 +0000

    update user_name to username in models

--- dcfideas/models.py
+++ dcfideas/models.py
@@ -29,9 +29,9 @@ class Idea(db.Model):
 class Users(db.Model):
     # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(260), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self.user_name
+        return self.username
