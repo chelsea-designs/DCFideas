509d1279c24ceb9577b7c78e28cc93f9b3bc9c49
chelsea-designs <hello@chelsea-designs.com>
2022-07-04 18:17:30 +0000

    remove debug from env

--- dcfideas/__init__.py
+++ dcfideas/__init__.py
@@ -11,9 +11,9 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 if os.environ.get("DEVELOPMENT") == "True":
