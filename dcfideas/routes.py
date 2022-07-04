d287318b793d1dcf79121ee61ca74b76f3f664ad
chelsea-designs <hello@chelsea-designs.com>
2022-07-04 16:26:51 +0000

    update heroku config with mongo uri

--- dcfideas/routes.py
+++ dcfideas/routes.py
@@ -8,31 +8,10 @@ from dcfideas.models import Strand, Idea, Users
 def home():
     return render_template("index.html")
 
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-    # if request.method == "POST":
-    #     # check if username already exists in db
-    #     existing_user = Users.query.filter(Users.username == \
-    #                                        request.form.get("username").lower()).all()
-        
-    #     if existing_user:
-    #         flash("Username already exists")
-    #         return redirect(url_for("register"))
-        
-    #     user = Users(
-    #         username=request.form.get("username").lower(),
-    #         password=generate_password_hash(request.form.get("password"))
-    #     )
-        
-    #     db.session.add(user)
-    #     db.session.commit()
-
-    #     # put the new user into 'session' cookie
-    #     session["user"] = request.form.get("username").lower()
-    #     flash("Registration Successful!")
-    #     return redirect(url_for("profile", username=session["user"]))
-
-    # return render_template("register.html")
+# @app.route("/search", methods=["GET", "POST"])
+# def search():
+#     ideas = 
+#     return render_template("ideas.html", ideas=ideas)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
