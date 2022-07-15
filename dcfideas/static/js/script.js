9d831835e532830cd45e72fcdc8fbb2b6f9a0ed0
chelsea-designs <hello@chelsea-designs.com>
2022-07-15 21:02:34 +0000

    remove unused vendor files and css

--- dcfideas/static/js/script.js
+++ dcfideas/static/js/script.js
@@ -220,12 +220,6 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfokio-lightbox'
-  });
 
   /**
    * Portfolio details slider
@@ -240,34 +234,6 @@
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
-      }
     }
   });
 
